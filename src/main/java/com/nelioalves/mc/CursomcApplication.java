bb8181cb7ece4b0e6f6785721b1200adc8d48891
Larissa Cruz <mlp-larissa@hotmail.com>
2021-07-19 01:07:02 -0300

    Estado e Cidade

--- src/main/java/com/nelioalves/mc/CursomcApplication.java
+++ src/main/java/com/nelioalves/mc/CursomcApplication.java
@@ -1,8 +1,13 @@
 package com.nelioalves.mc;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
+import com.nelioalves.mc.domain.Cidade;
+import com.nelioalves.mc.domain.Estado;
 import com.nelioalves.mc.domain.Produto;
+import com.nelioalves.mc.repository.CidadeRepository;
+import com.nelioalves.mc.repository.EstadoRepository;
 import com.nelioalves.mc.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -18,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner {
     private CategoriaRepository categoriaRepository;
     @Autowired
     private ProdutoRepository produtoRepository;
+    @Autowired
+    private EstadoRepository estadoRepository;
+    @Autowired
+    private CidadeRepository cidadeRepository;
 
 
     public static void main(String[] args) {
@@ -44,6 +53,21 @@ public class CursomcApplication implements CommandLineRunner {
 
         categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
         produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+        Estado est1 = new Estado(null, "Minas Gerais");
+        Estado est2 = new Estado(null, "São Paulo");
+
+        Cidade c1 = new Cidade(null, "Uberlândia", est1);
+        Cidade c2 = new Cidade(null, "São Paulo", est2);
+        Cidade c3 = new Cidade(null, "Campinas", est2);
+
+        est1.getCidades().addAll(Arrays.asList(c1));
+        est2.getCidades().addAll(Arrays.asList(c2,c3));
+
+        estadoRepository.saveAll(Arrays.asList(est1, est2));
+        cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+
 
     }
 
---
+++ src/main/java/com/nelioalves/mc/repository/EstadoRepository.java
@@ -0,0 +1,13 @@
+package com.nelioalves.mc.repository;
+
+import com.nelioalves.mc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado,Integer> {
+
+	
+	
+}
+																					
